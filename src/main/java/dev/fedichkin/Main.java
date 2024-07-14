d5f7267f09467344d8d3e45cbe2d70a4ad561cd3
Alekseu F <Tapo4ek@gmail.com>
2024-07-14 18:35:32 +0300

    промежуточный результат

--- src/main/java/dev/fedichkin/Main.java
+++ src/main/java/dev/fedichkin/Main.java
@@ -1,25 +1,28 @@
 package dev.fedichkin;
 
+import com.sun.net.httpserver.Request;
+
+import java.io.BufferedOutputStream;
+
+
 public class Main {
-    public class Main {
-        public static void main(String[] args){
-            final var server = new Server();
-            // код инициализации сервера (из вашего предыдущего ДЗ)
+    public static void main(String[] args) {
+        final var server = new Server(9999);
 
-            // добавление хендлеров (обработчиков)
-            server.addHandler("GET", "/messages", new Handler() {
-                public void handle(Request request, BufferedOutputStream responseStream) {
-                    // TODO: handlers code
-                }
-            });
-            server.addHandler("POST", "/messages", new Handler() {
-                public void handle(Request request, BufferedOutputStream responseStream) {
-                    // TODO: handlers code
-                }
-            });
+        // добавление хендлеров (обработчиков)
+        server.addHandler("GET", "/messages", new Handler() {
+            public void handle(Request request, BufferedOutputStream responseStream) {
 
-            server.listen(9999);
-        }
+            }
+        });
+        server.addHandler("POST", "/messages", new Handler() {
+            public void handle(Request request, BufferedOutputStream responseStream) {
+
+            }
+        });
+
+        server.startServer();
     }
 }
+
 
---
+++ src/main/java/dev/fedichkin/Request.java
@@ -0,0 +1,25 @@
+package dev.fedichkin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Request {
+    private final String path;
+    private final Map<String, String> queryParams = new HashMap<>();
+
+    public Request(String path) {
+        this.path = path;
+    }
+
+    public void addQueryParam(String key, String value) {
+        queryParams.put(key, value);
+    }
+
+    public String getQueryParam(String name) {
+        return queryParams.get(name);
+    }
+
+    public Map<String, String> getQueryParams() {
+        return queryParams;
+    }
+}
